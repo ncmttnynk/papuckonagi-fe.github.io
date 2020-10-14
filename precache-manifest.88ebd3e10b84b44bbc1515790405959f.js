a16f2b647376e663aa478692a88b56a3c72d990e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-14 19:26:44 +0000

    Deploy ncmttnynk/papuckonagi-fe.github.io to github.com/ncmttnynk/papuckonagi-fe.github.io.git:gh-pages

---
+++ precache-manifest.88ebd3e10b84b44bbc1515790405959f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff793f6cb3dca4cb5b40505fb2cf633",
+    "url": "/papuckonagi-fe.github.io/index.html"
+  },
+  {
+    "revision": "5828f49c30fbe8a32e43",
+    "url": "/papuckonagi-fe.github.io/static/css/2.6feb3358.chunk.css"
+  },
+  {
+    "revision": "3421c75cc8b37875358c",
+    "url": "/papuckonagi-fe.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5828f49c30fbe8a32e43",
+    "url": "/papuckonagi-fe.github.io/static/js/2.1d93e24b.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/papuckonagi-fe.github.io/static/js/2.1d93e24b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3421c75cc8b37875358c",
+    "url": "/papuckonagi-fe.github.io/static/js/main.8d0fb9e3.chunk.js"
+  },
+  {
+    "revision": "fdf6fe7d92d294f6e2b2",
+    "url": "/papuckonagi-fe.github.io/static/js/runtime-main.f03f1c56.js"
+  }
+]);
